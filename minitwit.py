0e72e1ec224a347abe5d027f708a243f39c4abf4
tcla <tcla@itu.dk>
2025-02-12 11:37:47 +0100

    decode gave errors

--- minitwit.py
+++ minitwit.py
@@ -39,7 +39,7 @@ def init_db():
     """Creates the database tables."""
     with closing(connect_db()) as db:
         with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read().decode('utf-8'))
+            db.cursor().executescript(f.read())#.decode('utf-8'))
         db.commit()
 
 
