4e4d706ade8f3067b4300c6ac5f0dfc7dd3af110
grumlebob <grumlebob@gmail.com>
2025-02-25 18:19:10 +0100

    add r to suppress warning

--- MiniTwitSolution/build_docker_images.sh
+++ MiniTwitSolution/build_docker_images.sh
@@ -7,7 +7,7 @@ docker build -t $DOCKER_USERNAME/clientimage -f MiniTwit.Client/Dockerfile .
 docker build -t $DOCKER_USERNAME/simulatorimage -f Simulator/Dockerfile ./Simulator
 
 echo "Login to Dockerhub, provide your password below..."
-read -s DOCKER_PASSWORD
+read -rs DOCKER_PASSWORD
 echo $DOCKER_PASSWORD | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 echo "Pushing Minitwit Images to Dockerhub..."
