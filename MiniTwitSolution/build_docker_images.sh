160473e950854193b9acd24c3a2085704ffaae2c
grumlebob <grumlebob@gmail.com>
2025-02-25 17:26:16 +0100

    add file that handles building images if not built

---
+++ MiniTwitSolution/build_docker_images.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+echo "Building Minitwit Images"
+docker build -t $DOCKER_USERNAME/migratorimage -f Dockerfile.migrator .
+docker build -t $DOCKER_USERNAME/apiimage -f MiniTwit.Api/Dockerfile .
+docker build -t $DOCKER_USERNAME/clientimage -f MiniTwit.Client/Dockerfile .
+docker build -t $DOCKER_USERNAME/simulatorimage -f Simulator/Dockerfile ./Simulator
+
+echo "Login to Dockerhub, provide your password below..."
+read -s DOCKER_PASSWORD
+echo $DOCKER_PASSWORD | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+echo "Pushing Minitwit Images to Dockerhub..."
+docker push $DOCKER_USERNAME/migratorimage:latest
+docker push $DOCKER_USERNAME/apiimage:latest
+docker push $DOCKER_USERNAME/clientimage:latest
+docker push $DOCKER_USERNAME/simulatorimage:latest
