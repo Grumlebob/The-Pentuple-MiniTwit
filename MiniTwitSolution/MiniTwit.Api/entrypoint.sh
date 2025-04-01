aacd2a81d4798e4f79fadf1bdb632a47026fea63
grumlebob <grumlebob@gmail.com>
2025-04-01 15:05:39 +0200

    update docker files for api

---
+++ MiniTwitSolution/MiniTwit.Api/entrypoint.sh
@@ -0,0 +1,12 @@
+﻿#!/bin/bash
+set -e
+
+echo "Waiting for database migrations to complete..."
+# Poll every 2 seconds until the /tmp/migrations_done file is found.
+# this is necessary since docker swarm cannot do depends_on 
+while [ ! -f /tmp/migrations_done ]; do
+    sleep 2
+done
+
+echo "Migrations completed. Starting the API..."
+exec dotnet MiniTwit.Api.dll
